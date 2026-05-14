bba9f0cdd62a9253392d1e735c7ca167e9629389
zhangyu <zhangyu34@gmail.com>
2026-05-14 16:49:42 +0800

    Jaeger and collector

--- otel.py
+++ otel.py
@@ -1,45 +1,27 @@
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, SpanExporter, SpanExportResult
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
 
-import json
-import os
-
-OTEL_FILE = "otel_trace.jsonl"
-
-
-class OverwriteFileSpanExporter(SpanExporter):
-    def __init__(self, filename):
-        self.filename = filename
-        # 👉 启动时清空文件（关键）
-        open(self.filename, "w").close()
-
-    def export(self, spans):
-        with open(self.filename, "a") as f:
-            for span in spans:
-                f.write(json.dumps({
-                    "name": span.name,
-                    "context": str(span.context),
-                    "attributes": dict(span.attributes or {}),
-                    "status": str(span.status),
-                }, default=str) + "\n")
-
-        return SpanExportResult.SUCCESS
-
-    def shutdown(self):
-        pass
-
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter
+)
 
 # --------------------
 # setup
 # --------------------
-provider = TracerProvider()
+resource = Resource(attributes={
+    "service.name": "mini-agent"
+})
 
-processor = BatchSpanProcessor(
-    OverwriteFileSpanExporter(OTEL_FILE)
-    # ConsoleSpanExporter()
-    
+provider = TracerProvider(resource=resource)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="localhost:4317",
+    insecure=True,   # 本地 Jaeger 不需要 TLS
 )
+
+processor = BatchSpanProcessor(otlp_exporter)
 
 provider.add_span_processor(processor)
 
